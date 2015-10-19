a71db5ac567241c75d8a42fa6f10605236c427ef
Donncha Finlay <donncha.finlay@gmail.com>
2015-10-19 17:11:31 +0200

    Add unit tests as core module

---
+++ core/src/main/java/matcom/dcf/com/recyctests/ConversionClass.java
@@ -0,0 +1,9 @@
+package matcom.dcf.com.recyctests;
+
+/**
+ * Created by dfinlay-air on 19/10/15.
+ */
+public class ConversionClass {
+
+
+}
---
+++ core/src/test/java/matcom/dcf/com/recyctests/ConversionClassTest.java
@@ -0,0 +1,19 @@
+package matcom.dcf.com.recyctests;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dfinlay-air on 19/10/15.
+ */
+public class ConversionClassTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testConvertValue() throws Exception {
+
+    }
+}
